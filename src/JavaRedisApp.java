71d901c7b2d1daa974d8b0e53c29c89517a78350
moronkreacionz <moronkreacionz@gmail.com>
2016-10-10 19:30:55 +0530

    added files commit

---
+++ src/JavaRedisApp.java
@@ -0,0 +1,13 @@
+import redis.clients.jedis.Jedis;
+
+public class JavaRedisApp{
+
+	public static void main(String args [])
+	{
+		Jedis jedis = new Jedis("redis"); 
+
+		while(true) {
+		System.out.println("Serer is running: " + jedis.ping()); 
+		}
+	}
+}
